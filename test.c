937eeac5fee679f98fae1078b8576b55287ee0cb
npvy <38617181+npvy@users.noreply.github.com>
2019-11-17 17:26:32 +0700

    Test for hook!
    Test for hook!

--- test.c
+++ test.c
@@ -6,10 +6,7 @@
 int main()
 {
     int fd = open("test.txt", O_WRONLY, S_IRUSR);
-
     printf("fd = %d\n", fd);
-
-    write(fd, "This will be output to testfile.txt\n", 36);
-
+    write(fd, "Hello Nguyen Phuong Vy!!!\n", 36);
     return 0;
 }
